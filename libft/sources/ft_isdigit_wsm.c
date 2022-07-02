85874132a355bce171908111b30015a00da087d2
Improvenss <gorkem_se_98@hotmail.com>
2022-07-02 19:23:17 +0300

    ft_is_digit.c turned original and ft_isdigit_wsm.c added

--- libft/sources/ft_isdigit_wsm.c
+++ libft/sources/ft_isdigit_wsm.c
@@ -1,21 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isdigit.c                                       :+:      :+:    :+:   */
+/*   ft_isdigit_wsm.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gsever <gsever@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/05 14:34:18 by gsever            #+#    #+#             */
-/*   Updated: 2022/07/02 19:20:24 by gsever           ###   ########.fr       */
+/*   Created: 2022/07/02 19:19:17 by gsever            #+#    #+#             */
+/*   Updated: 2022/07/02 19:22:27 by gsever           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libft.h"
 
-//Checking char; is it number return 0
+//Checking char; is it number or space or '-' return 0.
 int	ft_isdigit(int c)
 {
-	if (c >= '0' && c <= '9')
+	if ((c >= '0' && c <= '9') || c == ' ' || c == '-')
 		return (0);
 	return (1);
 }
